708e87d059aa48e0ae0205408ca50907bd7606c5
Adrian Walker <awalker3365@gmail.com>
2021-04-19 11:36:20 -0400

    2nd

---
+++ sort.js
@@ -0,0 +1,13 @@
+// 1.)
+const newNumbers = [21, 96, 7, 65, 2, 8, 17, 5, 33, 3]
+newNumbers.sort((a,b) => a - b)
+console.log(newNumbers)
+
+// 2.)
+const ages = [21, 96, 7, 65, 2, 8, 17, 5, 33, 3]
+ages.sort((a,b) => b - a)
+console.log(ages)
+
+// 3.)
+const sports = ["football", "baseball", "basketball", "soccer", "golf"]
+sports.sort((a,b) => b - a )
